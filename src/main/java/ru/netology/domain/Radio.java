7701b882fc0ab11dfa57b1d33dfd065436d5d6a2
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-01-17 13:27:26 +0300

    Added JACOCO report

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,3 +1,5 @@
+package ru.netology.domain;
+
 public class Radio {
 
     private int currentStation;
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,6 +1,8 @@
+package ru.netology.domain;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RadioTest {
 
